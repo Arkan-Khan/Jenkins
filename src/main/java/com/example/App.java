86258f54602d69456ede2e5de9e01cd11b502b56
Arkan <arkankhan051@gmail.com>
2025-02-13 20:55:34 +0530

    Initial commit again

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, Maven Using Webhooks!");
+        System.out.println("Hello 1 before WebHook!");
     }
 }
