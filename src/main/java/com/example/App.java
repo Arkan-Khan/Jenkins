8c4f92e477c10fbea0816afe3ed1307cab8b9c40
Arkan <arkankhan051@gmail.com>
2025-02-13 21:12:22 +0530

    Implemented Webhooks and Ngrok

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello 1 before WebHook!");
+        System.out.println("Hello from Maven Using Webhooks and Ngrok!");
     }
 }
